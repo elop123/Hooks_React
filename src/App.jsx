6c18363e91ca650eb5ccfa74ab7600afb1e0d2da
elop123 <142408289+elop123@users.noreply.github.com>
2024-08-23 13:20:34 +0200

    modal

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <>
       <Text />
       <Greeting />
+      <Modal />
     </>
   )
 }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Modal(){
+ return(
+    <>
+    <h1>hello</h1>
+    </>
+ )
+}
+
+export default Modal
