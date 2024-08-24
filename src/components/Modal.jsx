66a6fd4c8627b58c4bc537cec2ea88bbd4b84d74
elop123 <142408289+elop123@users.noreply.github.com>
2024-08-24 17:10:27 +0200

    modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,33 @@
 import { useState } from "react";
+import style from '../style/Modal.module.scss'
 
 function Modal(){
+
+   const[isOpen, setIsOpen] =useState(false);
+
+   function openModal(){
+      setIsOpen(true);
+   };
+
+   function closeModal(){
+      setIsOpen(false);
+   };
+
  return(
-    <>
-    <h1>hello</h1>
-    </>
+    <div>
+    <button className={style.modalBtn} onClick={openModal}>Open Modal</button>
+
+    {isOpen && (
+                <div className={style.modal} onClick={closeModal}>
+                 
+                    <div className={style.modalContent} >
+                        <span className={style.close} onClick={closeModal}>&times;</span>
+                        <p>Some text in the Modal..</p>
+                    </div>
+                </div>
+            )}
+            
+    </div>
  )
 }
 
